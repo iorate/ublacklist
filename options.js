e8f098ead1c6ecf0cecb0504ec12e27ec9773538
iorate <iorate@users.noreply.github.com>
2018-10-04 02:41:16 +0900

    Simplify options.js

--- options.js
+++ options.js
@@ -1,23 +1,9 @@
-class UBlacklistOptions {
-  constructor() {
-    for (const element of document.querySelectorAll('[data-translate]')) {
-      element.insertAdjacentHTML('beforeend', chrome.i18n.getMessage(element.dataset.translate));
-    }
-
-    chrome.storage.local.get({ blacklist: '' }, options => {
-      this.onBlacklistLoaded(options.blacklist);
-    });
-
-    document.getElementById('save').addEventListener('click', () => {
-      this.onSaveButtonClicked();
-    });
+chrome.storage.local.get({ blacklist: '' }, options => {
+  for (const element of document.querySelectorAll('[data-translate]')) {
+    element.insertAdjacentHTML('beforeend', chrome.i18n.getMessage(element.dataset.translate));
   }
-
-  onBlacklistLoaded(blacklist) {
-    document.getElementById('blacklist').value = blacklist;
-  }
-
-  onSaveButtonClicked() {
+  document.getElementById('blacklist').value = options.blacklist;
+  document.getElementById('save').addEventListener('click', () => {
     chrome.storage.local.set({ blacklist: document.getElementById('blacklist').value }, () => {
       const saveStatus = document.getElementById('saveStatus');
       saveStatus.style.display = 'inline';
@@ -25,7 +11,5 @@ class UBlacklistOptions {
         saveStatus.style.display = 'none';
       }, 1000);
     });
-  }
-}
-
-new UBlacklistOptions();
+  });
+});
