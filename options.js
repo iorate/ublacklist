4665128a5a6ccc1fffb501cbdc11081683542442
iorate <iorate@users.noreply.github.com>
2018-10-04 04:32:10 +0900

    Refactor options page

--- options.js
+++ options.js
@@ -1,7 +1,17 @@
 chrome.storage.local.get({ blacklist: '' }, options => {
-  for (const element of document.querySelectorAll('[data-translate]')) {
-    element.insertAdjacentHTML('beforeend', chrome.i18n.getMessage(element.dataset.translate));
-  }
+  document.body.insertAdjacentHTML('beforeend', `
+    <p>
+      ${_('blacklist')}<br>
+      <small>${_('blacklistDescription')}</small><br>
+      <small>${_('example')}: *://*.example.com/*</small><br>
+      <small>${_('example')}: /example\.(net|org)/</small><br>
+      <textarea id="blacklistInput" cols="80" rows="8" spellcheck="false"></textarea>
+    </p>
+    <p>
+      <button id="saveButton">${_('save')}</button>
+      <span id="saveStatusSpan" style="display:none">${_('saveStatus')}</span>
+    </p>
+  `);
 
   const blacklistInput = document.getElementById('blacklistInput');
   const saveButton = document.getElementById('saveButton');
