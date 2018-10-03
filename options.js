d084741ff28ea477e11829def2183da631a2e472
iorate <iorate@users.noreply.github.com>
2018-10-04 02:58:03 +0900

    Update ids of options page

--- options.js
+++ options.js
@@ -2,13 +2,18 @@ chrome.storage.local.get({ blacklist: '' }, options => {
   for (const element of document.querySelectorAll('[data-translate]')) {
     element.insertAdjacentHTML('beforeend', chrome.i18n.getMessage(element.dataset.translate));
   }
-  document.getElementById('blacklist').value = options.blacklist;
-  document.getElementById('save').addEventListener('click', () => {
-    chrome.storage.local.set({ blacklist: document.getElementById('blacklist').value }, () => {
-      const saveStatus = document.getElementById('saveStatus');
-      saveStatus.style.display = 'inline';
+
+  const blacklistInput = document.getElementById('blacklistInput');
+  const saveButton = document.getElementById('saveButton');
+  const saveStatusSpan = document.getElementById('saveStatusSpan');
+
+  blacklistInput.value = options.blacklist;
+
+  saveButton.addEventListener('click', () => {
+    chrome.storage.local.set({ blacklist: blacklistInput.value }, () => {
+      saveStatusSpan.style.display = 'inline';
       setTimeout(() => {
-        saveStatus.style.display = 'none';
+        saveStatusSpan.style.display = 'none';
       }, 1000);
     });
   });
