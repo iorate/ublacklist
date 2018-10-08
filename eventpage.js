67584ce48b5e31fc24d7fc7b38fd8fddfc9aaba8
iorate <iorate@users.noreply.github.com>
2018-10-08 20:30:10 +0900

    Remove redundant comment

--- eventpage.js
+++ eventpage.js
@@ -2,7 +2,6 @@ chrome.runtime.onInstalled.addListener(details => {
   if (details.reason == 'update') {
     const [x, y = 0, z = 0] = details.previousVersion.split('.').map(Number);
     if (x < 1 || x == 1 && (y < 2 || y == 2 && z < 3)) {
-      // previousVersion < 1.2.3
       chrome.storage.sync.get({ blacklist: '' }, options => {
         chrome.storage.local.set(options);
       });
