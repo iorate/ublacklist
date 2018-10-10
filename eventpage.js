ed358beae01ec2a0daf178341eb72e0321d021a3
iorate <iorate@users.noreply.github.com>
2018-10-11 00:45:50 +0900

    Rename: options -> items

--- eventpage.js
+++ eventpage.js
@@ -2,8 +2,8 @@ chrome.runtime.onInstalled.addListener(details => {
   if (details.reason == 'update') {
     const [x, y = 0, z = 0] = details.previousVersion.split('.').map(Number);
     if (x < 1 || x == 1 && (y < 2 || y == 2 && z < 3)) {
-      chrome.storage.sync.get({ blacklist: '' }, options => {
-        chrome.storage.local.set(options);
+      chrome.storage.sync.get({ blacklist: '' }, items => {
+        chrome.storage.local.set(items);
       });
     }
   }
