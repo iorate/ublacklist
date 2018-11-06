ed07acced133dbb57975180f76df492bd099e89a
iorate <iorate@users.noreply.github.com>
2018-11-07 03:11:11 +0900

    Fix: the sync storage was not cleared when updated from version >= 1.2.3

--- background.js
+++ background.js
@@ -1,9 +1,6 @@
 chrome.runtime.onInstalled.addListener(details => {
   if (details.reason == 'update') {
-    const [x, y = 0, z = 0] = details.previousVersion.split('.').map(Number);
-    if (x < 1 || x == 1 && (y < 2 || y == 2 && z < 3)) {
-      chrome.storage.sync.clear();
-    }
+    chrome.storage.sync.clear();
   }
 });
 
