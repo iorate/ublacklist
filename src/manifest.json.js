5431d0b32db8d53913e031bc56b6129ecd424071
iorate <iorate@users.noreply.github.com>
2019-11-07 23:53:47 +0900

    v3.2.1

--- src/manifest.json.js
+++ src/manifest.json.js
@@ -48,7 +48,7 @@ const manifest = {
     page: 'options.html',
   },
   permissions: ['activeTab', 'identity', 'storage'],
-  version: '3.2.0',
+  version: '3.2.1',
   content_scripts: [
     {
       css: ['css/content.css'],
