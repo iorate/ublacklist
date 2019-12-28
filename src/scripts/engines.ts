421c04d8ee87efd183f0e7f2a9c6eaee3b66082d
iorate <iorate@users.noreply.github.com>
2019-12-29 00:08:08 +0900

    Fix: did not work on some URLs of Startpage.com

--- src/scripts/engines.ts
+++ src/scripts/engines.ts
@@ -4,9 +4,13 @@ export const ENGINES: Engine[] = [
   {
     id: 'startpage',
     matches: [
+      'https://www.startpage.com/do/dsearch?*',
+      'https://www.startpage.com/do/metasearch.pl?*',
       'https://www.startpage.com/do/search',
-      'https://www.startpage.com/rvd/search',
+      'https://www.startpage.com/do/search?*',
+      'https://www.startpage.com/rvd/search?*',
       'https://www.startpage.com/sp/search',
+      'https://www.startpage.com/sp/search?*',
     ],
     name: 'Startpage.com',
   },
