070911f13cea40cb0c3963e28cac8aa9157f02f0
iorate <iorate@users.noreply.github.com>
2020-03-02 14:58:54 +0900

    v3.4.5

--- src/manifest.json.js
+++ src/manifest.json.js
@@ -49,7 +49,7 @@ const manifest = {
     page: 'options.html',
   },
   permissions: ['activeTab', 'alarms', 'identity', 'storage'],
-  version: '3.4.4',
+  version: '3.4.5',
   content_scripts: [
     {
       css: ['styles/engines/google.css', 'styles/content.css'],
