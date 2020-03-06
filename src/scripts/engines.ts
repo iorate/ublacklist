c4d3320a05a423b1d44138a10c1fd84649131842
iorate <iorate@users.noreply.github.com>
2020-03-07 00:03:59 +0900

    Add 'https://startpage.com/*' to the Startpage URLs

--- src/scripts/engines.ts
+++ src/scripts/engines.ts
@@ -17,13 +17,11 @@ export const ENGINES: Engine[] = [
     id: 'startpage',
     name: 'Startpage.com',
     matches: [
-      'https://www.startpage.com/do/dsearch?*',
-      'https://www.startpage.com/do/metasearch.pl?*',
-      'https://www.startpage.com/do/search',
-      'https://www.startpage.com/do/search?*',
-      'https://www.startpage.com/rvd/search?*',
-      'https://www.startpage.com/sp/search',
-      'https://www.startpage.com/sp/search?*',
+      'https://startpage.com/do/*',
+      'https://startpage.com/sp/*',
+      'https://www.startpage.com/do/*',
+      'https://www.startpage.com/rvd/*',
+      'https://www.startpage.com/sp/*',
     ],
   },
 ];
