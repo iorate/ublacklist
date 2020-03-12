b2c3ec3aa4c54f8ed986e40cc2f5ce91b1a8c0aa
iorate <iorate@users.noreply.github.com>
2020-03-13 02:19:05 +0900

    chore: install semantic-release-chrome

--- release.config.js
+++ release.config.js
@@ -2,6 +2,23 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/github',
+    [
+      'semantic-release-chrome',
+      {
+        asset: 'ublacklist-chrome.zip',
+        distFolder: 'dist/chrome/production',
+        extensionId: 'pncfbmialoiaghdehhbnbhkkgmjanfhe',
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          {
+            path: 'ublacklist-chrome.zip',
+          },
+        ],
+      },
+    ],
   ],
 };
