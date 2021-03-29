594c1f6c93ab05effb67004cfb5642780f6fab15
Luka <rainfyou@gmail.com>
2021-03-29 10:53:43 +0800

    fix git

---
+++ safari-project/scripts/add_locale.rb
@@ -0,0 +1,35 @@
+require 'xcodeproj'
+require 'fileutils'
+
+if ARGV.length != 2
+    puts 'arguments error'
+    return
+end
+
+from = ARGV[0]
+to = ARGV[1]
+
+# copy localizable file
+from_dir = "../uBlacklist for Safari/Common/Intl/#{from}.lproj"
+to_dir = "../uBlacklist for Safari/Common/Intl/#{to}.lproj"
+FileUtils.copy_entry(from_dir, to_dir)
+
+project_file = '../uBlacklist for Safari.xcodeproj'
+
+puts "Processing #{project_file} ..."
+
+project = Xcodeproj::Project.open(project_file)
+
+intl_group = project.main_group.find_subpath('uBlacklist for Safari/Common/Intl/Localizable.strings')
+
+if intl_group.find_subpath("#{to}")
+    puts "#{to} reference has already exist"
+    return
+end
+
+ref = intl_group.new_reference("#{to}.lproj/Localizable.strings")
+ref.name = to
+ref.include_in_index = nil
+project.save
+
+puts "Successfully added #{to}.lproj/Localizable.strings reference"
---
+++ safari-project/scripts/change_version.rb
@@ -0,0 +1,37 @@
+require 'xcodeproj'
+require 'json'
+
+if ARGV.length != 2
+    puts 'arguments error'
+    return
+end
+
+version = ARGV[0]
+build_number = ARGV[1]
+
+project_file = '../uBlacklist for Safari.xcodeproj'
+
+puts "Processing #{project_file} ..."
+
+project = Xcodeproj::Project.open(project_file)
+
+core_manifest_file_path = '../../dist/safari/production/manifest.json'
+
+if File.exists?(core_manifest_file_path) == false
+    puts 'uBlacklist-Core has not been generated yet'
+    return
+end
+
+manifest = JSON.parse(File.read(core_manifest_file_path))
+manifest['version'] = version
+file = File.open(core_manifest_file_path, 'w')
+file.write(JSON.pretty_generate(manifest))
+
+project.targets.each do |target|
+    target.build_configuration_list.set_setting('MARKETING_VERSION', version)
+    target.build_configuration_list.set_setting('CURRENT_PROJECT_VERSION', build_number)
+end
+
+project.save
+
+puts "current version: #{version}, current build number: #{build_number}"
