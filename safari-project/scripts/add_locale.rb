c58bdf3d1da72f065ce0381a73a1f5c1e5d1bba0
Luka <rainfyou@gmail.com>
2021-09-19 16:13:59 +0800

    update add_locale.rb

--- safari-project/scripts/add_locale.rb
+++ safari-project/scripts/add_locale.rb
@@ -9,10 +9,30 @@ end
 from = ARGV[0]
 to = ARGV[1]
 
+def group_new_reference project, subpath, to_locale
+    intl_group = project.main_group.find_subpath(subpath)
+
+    if intl_group.find_subpath("#{to_locale}")
+        puts "#{to_locale} reference has already exist"
+        return
+    end
+
+    ref = intl_group.new_reference("#{to_locale}.lproj/Localizable.strings")
+    ref.name = to_locale
+    ref.include_in_index = nil
+    project.save
+
+    puts "Successfully added #{to_locale}.lproj/Localizable.strings reference"
+end
+
 # copy localizable file
-from_dir = "../uBlacklist for Safari/Common/Intl/#{from}.lproj"
-to_dir = "../uBlacklist for Safari/Common/Intl/#{to}.lproj"
-FileUtils.copy_entry(from_dir, to_dir)
+osx_from_dir = "../macOS (App)/Common/Intl/#{from}.lproj"
+osx_to_dir = "../macOS (App)/Common/Intl/#{to}.lproj"
+FileUtils.copy_entry(osx_from_dir, osx_to_dir)
+
+ios_from_dir = "../iOS (App)/Common/Intl/#{from}.lproj"
+ios_to_dir = "../iOS (App)/Common/Intl/#{to}.lproj"
+FileUtils.copy_entry(ios_from_dir, ios_to_dir)
 
 project_file = '../uBlacklist for Safari.xcodeproj'
 
@@ -20,16 +40,7 @@ puts "Processing #{project_file} ..."
 
 project = Xcodeproj::Project.open(project_file)
 
-intl_group = project.main_group.find_subpath('uBlacklist for Safari/Common/Intl/Localizable.strings')
+group_new_reference(project, 'macOS (App)/Common/Intl/Localizable.strings', to)
+group_new_reference(project, 'iOS (App)/Common/Intl/Localizable.strings', to)
 
-if intl_group.find_subpath("#{to}")
-    puts "#{to} reference has already exist"
-    return
-end
-
-ref = intl_group.new_reference("#{to}.lproj/Localizable.strings")
-ref.name = to
-ref.include_in_index = nil
-project.save
-
-puts "Successfully added #{to}.lproj/Localizable.strings reference"
+puts "Complete."
