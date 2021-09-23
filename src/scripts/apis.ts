92186451b550907958fb2e7276e597bda77e2761
iorate <682043+iorate@users.noreply.github.com>
2021-09-23 18:34:49 +0900

    fix(ios): work around the sync problem
    re #99

--- src/scripts/apis.ts
+++ src/scripts/apis.ts
@@ -6,10 +6,6 @@ export namespace apis {
     export type Alarm = chrome.alarms.Alarm;
     export type _CreateAlarmInfo = chrome.alarms.AlarmCreateInfo;
 
-    export function create(name: string, alarmInfo: _CreateAlarmInfo): void {
-      chrome.alarms.create(name, alarmInfo);
-    }
-
     export function clear(name: string): Promise<boolean> {
       return new Promise<boolean>((resolve, reject) => {
         chrome.alarms.clear(name, wasCleared => {
@@ -17,6 +13,22 @@ export namespace apis {
             reject(new Error(chrome.runtime.lastError.message));
           } else {
             resolve(wasCleared);
+          }
+        });
+      });
+    }
+
+    export function create(name: string, alarmInfo: _CreateAlarmInfo): void {
+      chrome.alarms.create(name, alarmInfo);
+    }
+
+    export function get(name: string): Promise<Alarm | undefined> {
+      return new Promise<Alarm | undefined>((resolve, reject) => {
+        chrome.alarms.get(name, alarm => {
+          if (chrome.runtime.lastError) {
+            reject(new Error(chrome.runtime.lastError.message));
+          } else {
+            resolve(alarm);
           }
         });
       });
@@ -310,6 +322,16 @@ export namespace apis {
         callback: (tabId: number, changeInfo: _OnUpdatedChangeInfo, tab: Tab) => void,
       ): void {
         chrome.tabs.onUpdated.removeListener(callback);
+      },
+    };
+  }
+
+  export namespace windows {
+    export const WINDOW_ID_NONE = chrome.windows.WINDOW_ID_NONE;
+
+    export const onFocusChanged = {
+      addListener(callback: (windowId: number) => void): void {
+        chrome.windows.onFocusChanged.addListener(callback);
       },
     };
   }
