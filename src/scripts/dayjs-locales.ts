a3590a41eeb321c0904fef0bb773f03729330af0
DeciBelioS <96150975+Deci8BelioS@users.noreply.github.com>
2021-12-17 18:50:31 +0100

    feat: add esp-translation  (#161)
    * esp-translation
    
    * esp-translation update captures
    
    * import languaje spanish
    
    * run yarn fix

--- src/scripts/dayjs-locales.ts
+++ src/scripts/dayjs-locales.ts
@@ -4,5 +4,6 @@ import 'dayjs/locale/ko';
 import 'dayjs/locale/ru';
 import 'dayjs/locale/tr';
 import 'dayjs/locale/uk';
+import 'dayjs/locale/es';
 import 'dayjs/locale/zh-cn';
 import 'dayjs/locale/zh-tw';
