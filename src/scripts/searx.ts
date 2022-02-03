65150f06f6083c086d52e6755600baef6ad43bbd
masterofobzene <94469116+masterofobzene@users.noreply.github.com>
2022-02-03 12:23:30 -0300

    replaced file on src\scripts\search-engines\searx.ts

---
+++ src/scripts/searx.ts
@@ -0,0 +1,49 @@
+import { SEARCH_ENGINES } from '../../common/search-engines';
+import { SearchEngine, SerpHandler } from '../types';
+import { handleSerp } from './helpers';
+
+function getSerpHandler(): SerpHandler {
+  return handleSerp({
+    globalStyle: {
+      '[data-ub-blocked="visible"]': {
+        backgroundColor: 'var(--ub-block-color, rgba(255, 192, 192, 0.5)) !important',
+      },
+      '.ub-button': {
+        color: 'var(--ub-link-color, rgb(0, 100, 77))',
+      },
+      '.ub-button:hover': {
+        textDecoration: 'underline',
+      },
+    },
+    controlHandlers: [
+      {
+        target: 'body',
+        position: 'afterbegin',
+        style: {
+          display: 'block',
+          fontSize: '13px',
+          padding: '9px 20px',
+          textAlign: 'right',
+        },
+      },
+    ],
+    entryHandlers: [
+      {
+        target: '.result',
+        url: 'a',
+        title: 'a',
+        actionTarget: root => root.querySelector('.result__body') || root,
+        actionStyle: {
+          display: 'block',
+          fontSize: '13px',
+          paddingTop: '5px',
+        },
+      },
+    ],
+  });
+}
+
+export const searx: Readonly<SearchEngine> = {
+  ...SEARCH_ENGINES.searx,
+  getSerpHandler,
+};
