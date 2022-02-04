cd56f187b04eb11bdd2abb8390a969038edca372
masterofobzene <94469116+masterofobzene@users.noreply.github.com>
2022-02-04 11:41:44 -0300

    ran lint

--- src/common/search-engines.ts
+++ src/common/search-engines.ts
@@ -356,7 +356,7 @@ export const SEARCH_ENGINES: Readonly<Record<SearchEngineId, Readonly<SearchEngi
           'https://search.antonkling.se/search?*',
           'https://searx.mxchange.org/search?*',
           'https://jsearch.pw/search?*',
-          'https://searx.org/search?*',         
+          'https://searx.org/search?*',
           'https://search.neet.works/search?*',
         ],
         //New public domains will have to be supported by the community.
