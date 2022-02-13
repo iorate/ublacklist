6ddd3c7ff6395e6ef4b72ed95dfa81ab265c803c
iorate <682043+iorate@users.noreply.github.com>
2022-02-14 00:42:55 +0900

    refactor: prefer React.VFC to () => JSX.Element

--- website/src/pages/callback.tsx
+++ website/src/pages/callback.tsx
@@ -3,31 +3,29 @@ import CodeBlock from '@theme/CodeBlock';
 import Layout from '@theme/Layout';
 import React from 'react';
 
-function Callback(): JSX.Element {
-  return (
-    <Layout title="Authentication Result">
-      <main className="container margin-vert--lg">
-        <BrowserOnly>
-          {() => {
-            const params = Object.fromEntries(new URL(window.location.href).searchParams);
-            return params.code != null ? (
-              <>
-                <h1>Authetication Succeeded</h1>
-                <p>Please copy the authorization code below and paste it in the options page.</p>
-                <CodeBlock>{params.code}</CodeBlock>
-              </>
-            ) : (
-              <>
-                <h1>Authentication Failed</h1>
-                <p>Please check the error message below.</p>
-                <CodeBlock className="language-json">{JSON.stringify(params, null, 2)}</CodeBlock>
-              </>
-            );
-          }}
-        </BrowserOnly>
-      </main>
-    </Layout>
-  );
-}
+const Callback: React.VFC = () => (
+  <Layout title="Authentication Result">
+    <main className="container margin-vert--lg">
+      <BrowserOnly>
+        {() => {
+          const params = Object.fromEntries(new URL(window.location.href).searchParams);
+          return params.code != null ? (
+            <>
+              <h1>Authetication Succeeded</h1>
+              <p>Please copy the authorization code below and paste it in the options page.</p>
+              <CodeBlock>{params.code}</CodeBlock>
+            </>
+          ) : (
+            <>
+              <h1>Authentication Failed</h1>
+              <p>Please check the error message below.</p>
+              <CodeBlock className="language-json">{JSON.stringify(params, null, 2)}</CodeBlock>
+            </>
+          );
+        }}
+      </BrowserOnly>
+    </main>
+  </Layout>
+);
 
 export default Callback;
