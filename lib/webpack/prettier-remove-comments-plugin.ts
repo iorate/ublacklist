b2fdd6ea53671c57bbaa4b4bc1cd62afc6f8af80
iorate <682043+iorate@users.noreply.github.com>
2023-01-07 23:30:00 +0900

    refactor: replace Custom Parser API with Plugin API (prettier)
    https://prettier.io/docs/en/api.html#custom-parser-api-deprecated

--- lib/webpack/prettier-remove-comments-plugin.ts
+++ lib/webpack/prettier-remove-comments-plugin.ts
@@ -1,12 +1,13 @@
 import prettier from 'prettier';
 import webpack from 'webpack';
+import { removeCommentsPlugin } from '../prettier/remove-comments-plugin';
 
-export class PrettierPlugin implements webpack.WebpackPluginInstance {
+export class PrettierRemoveCommentsPlugin implements webpack.WebpackPluginInstance {
   apply(compiler: webpack.Compiler): void {
-    compiler.hooks.thisCompilation.tap(PrettierPlugin.name, compilation => {
+    compiler.hooks.thisCompilation.tap(PrettierRemoveCommentsPlugin.name, compilation => {
       compilation.hooks.processAssets.tap(
         {
-          name: PrettierPlugin.name,
+          name: PrettierRemoveCommentsPlugin.name,
           // Run after html-webpack-plugin
           stage: webpack.Compilation.PROCESS_ASSETS_STAGE_OPTIMIZE_INLINE + 1,
         },
@@ -22,26 +23,28 @@ export class PrettierPlugin implements webpack.WebpackPluginInstance {
     const options = prettier.resolveConfig.sync(__filename);
     if (!options) {
       compilation.errors.push(
-        new webpack.WebpackError(`${PrettierPlugin.name}: no prettier config was found`),
+        new webpack.WebpackError(
+          `${PrettierRemoveCommentsPlugin.name}: no prettier config was found`,
+        ),
       );
       return;
     }
     for (const [name, source] of Object.entries(assets)) {
-      const parser: prettier.RequiredOptions['parser'] | null = name.endsWith('.js')
-        ? (text, { meriyah }) => {
-            const ast = meriyah(text) as { comments?: unknown };
-            delete ast.comments;
-            return ast;
-          }
+      const parser: string | null = name.endsWith('.js')
+        ? 'meriyah-remove-comments'
         : name.endsWith('.json')
         ? 'json'
         : name.endsWith('.html')
         ? 'html'
         : null;
-      if (!parser) {
+      if (parser == null) {
         continue;
       }
-      const formattedCode = prettier.format(source.source().toString(), { ...options, parser });
+      const formattedCode = prettier.format(source.source().toString(), {
+        ...options,
+        parser,
+        plugins: [removeCommentsPlugin],
+      });
       compilation.updateAsset(name, new webpack.sources.RawSource(formattedCode));
     }
   }
