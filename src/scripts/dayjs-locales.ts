0590967fe73aaa05f7ca47748ff178c8ee425c21
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-03-30 14:11:38 +0200

    Add pl dayjs locale

--- src/scripts/dayjs-locales.ts
+++ src/scripts/dayjs-locales.ts
@@ -1,9 +1,10 @@
 import 'dayjs/locale/de';
+import 'dayjs/locale/es';
 import 'dayjs/locale/ja';
 import 'dayjs/locale/ko';
+import 'dayjs/locale/pl';
 import 'dayjs/locale/ru';
 import 'dayjs/locale/tr';
 import 'dayjs/locale/uk';
-import 'dayjs/locale/es';
 import 'dayjs/locale/zh-cn';
 import 'dayjs/locale/zh-tw';
