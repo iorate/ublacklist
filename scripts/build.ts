46dbdc487c4dbb6d7d54d41488d1bfd4587eeb6f
iorate <682043+iorate@users.noreply.github.com>
2024-04-30 20:34:02 +0900

    build: update build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,13 +1,13 @@
-import fs from "node:fs/promises";
 import module from "node:module";
+import os from "node:os";
 import path from "node:path";
-import url from "node:url";
 import util from "node:util";
 import chalk from "chalk";
 import * as chokidar from "chokidar";
 import dayjs from "dayjs";
 import * as dotenv from "dotenv";
 import * as esbuild from "esbuild";
+import fs from "fs-extra";
 import { globby } from "globby";
 import pLimit from "p-limit";
 import { z } from "zod";
@@ -19,11 +19,50 @@ type Context = {
   watch: boolean;
   srcDir: string;
   destDir: string;
-  define: Record<string, string>;
+  tempDir: string;
 };
 
-async function copyAssets({ browser, watch, srcDir, destDir }: Context) {
-  const files = [
+function createGetUpdatedFiles(files: string[]): () => Promise<string[]> {
+  let mtimes: [string, number | null][] = files.map((file) => [file, null]);
+  return async () => {
+    const updatedFiles: string[] = [];
+    mtimes = await Promise.all(
+      mtimes.map(async ([file, mtime]) => {
+        const { mtimeMs: newMtime } = await fs.stat(file);
+        if (newMtime !== mtime) {
+          updatedFiles.push(file);
+        }
+        return [file, newMtime];
+      }),
+    );
+    return updatedFiles;
+  };
+}
+
+function defineProcessEnv(context: Context): Record<string, string> {
+  const { browser, version, debug, watch } = context;
+  const vars = {
+    NODE_ENV: debug ? "development" : "production",
+    BROWSER: browser,
+    VERSION: version,
+    DEBUG: debug ? "true" : "false",
+    WATCH: watch ? "true" : "false",
+    DROPBOX_API_KEY: process.env.DROPBOX_API_KEY,
+    DROPBOX_API_SECRET: process.env.DROPBOX_API_SECRET,
+    GOOGLE_DRIVE_API_KEY: process.env.GOOGLE_DRIVE_API_KEY,
+    GOOGLE_DRIVE_API_SECRET: process.env.GOOGLE_DRIVE_API_SECRET,
+  };
+  return Object.fromEntries(
+    Object.entries(vars).map(([key, value]) => [
+      `process.env.${key}`,
+      JSON.stringify(value),
+    ]),
+  );
+}
+
+async function createCopyFiles(context: Context): Promise<() => Promise<void>> {
+  const { browser, watch, srcDir, destDir } = context;
+  const sources = [
     ...(browser === "safari"
       ? ["icons/template-icon-32.png"]
       : ["icons/icon-32.png"]),
@@ -35,58 +74,90 @@ async function copyAssets({ browser, watch, srcDir, destDir }: Context) {
     "scripts/active.js",
     "third-party-notices.txt",
   ];
-  await Promise.all(
-    files.map(async (file) => {
-      const src = path.join(srcDir, file);
-      const dest = path.join(destDir, file);
-      await fs.mkdir(path.dirname(dest), { recursive: true });
-      await fs.copyFile(src, dest);
-    }),
-  );
+  if (watch) {
+    const getUpdateSources = createGetUpdatedFiles(
+      sources.map((src) => path.join(srcDir, src)),
+    );
+    return async () => {
+      const updatedSources = await getUpdateSources();
+      await Promise.all(
+        updatedSources.map((src) =>
+          fs.copy(src, path.join(destDir, path.relative(srcDir, src))),
+        ),
+      );
+    };
+  }
+  return async () => {
+    await Promise.all(
+      sources.map((src) =>
+        fs.copy(path.join(srcDir, src), path.join(destDir, src)),
+      ),
+    );
+  };
 }
 
-async function buildJsonTs({ srcDir, destDir, define }: Context) {
-  const files = [
+async function createBuildJSON(context: Context): Promise<() => Promise<void>> {
+  const { watch, srcDir, destDir, tempDir } = context;
+  const sources = [
     ...(await globby("_locales/*/messages.json.ts", { cwd: srcDir })),
     "manifest.json.ts",
   ];
-  await esbuild.build({
+  const esbuildOptions: esbuild.BuildOptions = {
     bundle: true,
-    define,
-    entryPoints: files.map((file) => path.join(srcDir, file)),
+    define: defineProcessEnv(context),
+    entryPoints: sources.map((src) => path.join(srcDir, src)),
     format: "cjs",
     logLevel: "silent",
     outExtension: { ".js": ".cjs" },
     outbase: srcDir,
-    outdir: destDir,
-  });
+    outdir: tempDir,
+  };
   const require = module.createRequire(import.meta.url);
-  await Promise.all(
-    files.map(async (file) => {
-      const dest = path.join(destDir, file.slice(0, -3)); // Remove ".ts"
-      const destCjsRelative = path.relative(
-        path.dirname(url.fileURLToPath(import.meta.url)),
-        `${dest}.cjs`,
-      ); // Relative to this script
-      delete require.cache[require.resolve(destCjsRelative)]; // Invalidate cache
-      const { default: content } = z
-        .object({ default: z.unknown() })
-        .parse(require(destCjsRelative));
-      await fs.rm(`${dest}.cjs`);
-      await fs.writeFile(dest, `${JSON.stringify(content, null, 2)}\n`);
-    }),
-  );
+  const requireModule = (mod: string): string => {
+    const modulePath = path.resolve(mod);
+    delete require.cache[require.resolve(modulePath)];
+    const exports: unknown = require(modulePath);
+    const { default: defaultExport } = z
+      .object({ default: z.unknown() })
+      .parse(exports);
+    return `${JSON.stringify(defaultExport, null, 2)}\n`;
+  };
+  if (watch) {
+    const esbuildContext = await esbuild.context(esbuildOptions);
+    const getUpdatedModules = createGetUpdatedFiles(
+      sources.map((src) => path.join(tempDir, `${src.slice(0, -3)}.cjs`)),
+    );
+    return async () => {
+      await esbuildContext.rebuild();
+      const updatedModules = await getUpdatedModules();
+      await Promise.all(
+        updatedModules.map((mod) =>
+          fs.outputFile(
+            path.join(destDir, `${path.relative(tempDir, mod.slice(0, -4))}`),
+            requireModule(mod),
+          ),
+        ),
+      );
+    };
+  }
+  return async () => {
+    await esbuild.build(esbuildOptions);
+    await Promise.all(
+      sources.map(async (src) =>
+        fs.outputFile(
+          path.join(destDir, `${src.slice(0, -3)}`),
+          requireModule(path.join(tempDir, `${src.slice(0, -3)}.cjs`)),
+        ),
+      ),
+    );
+  };
 }
 
-async function buildTs({
-  browser,
-  debug,
-  watch,
-  srcDir,
-  destDir,
-  define,
-}: Context) {
-  const files = [
+async function createBuildScripts(
+  context: Context,
+): Promise<() => Promise<void>> {
+  const { browser, debug, watch, srcDir, destDir } = context;
+  const sources = [
     "scripts/background.ts",
     "scripts/content-script.tsx",
     "scripts/options.tsx",
@@ -95,10 +166,10 @@ async function buildTs({
       ? ["scripts/watch.ts", "scripts/watch-worker.ts"]
       : []),
   ];
-  await esbuild.build({
+  const esbuildOptions: esbuild.BuildOptions = {
     bundle: true,
-    define,
-    entryPoints: files.map((file) => path.join(srcDir, file)),
+    define: defineProcessEnv(context),
+    entryPoints: sources.map((file) => path.join(srcDir, file)),
     format: "esm",
     jsx: "automatic",
     jsxDev: debug,
@@ -108,44 +179,46 @@ async function buildTs({
     outbase: srcDir,
     outdir: destDir,
     sourcemap: debug,
-  });
+  };
+  if (watch) {
+    const esbuildContext = await esbuild.context(esbuildOptions);
+    return async () => {
+      await esbuildContext.rebuild();
+    };
+  }
+  return async () => {
+    await esbuild.build(esbuildOptions);
+  };
 }
 
-async function build(context: Context) {
-  if (context.watch) {
-    process.stdout.write(`[${dayjs().format("HH:mm:ss")}] `);
-  }
-  process.stdout.write("Building... ");
-  try {
-    await Promise.all([
-      copyAssets(context),
-      buildJsonTs(context),
-      buildTs(context),
-    ]);
-    if (context.watch && context.browser === "chrome") {
-      await fs.writeFile(path.join(context.destDir, ".watch"), "");
+async function createBuild(context: Context): Promise<() => Promise<void>> {
+  const { browser, watch, destDir } = context;
+  const [copyFiles, buildJSON, buildScripts] = await Promise.all([
+    createCopyFiles(context),
+    createBuildJSON(context),
+    createBuildScripts(context),
+  ]);
+  return async () => {
+    if (watch) {
+      process.stdout.write(`[${dayjs().format("HH:mm:ss")}] `);
     }
-    process.stdout.write(chalk.green("✔\n"));
-  } catch (error) {
-    process.stdout.write(chalk.red("✘\n"));
-    if (error instanceof Error) {
-      process.stderr.write(`${error.toString()}\n`);
+    process.stdout.write("Building... ");
+    try {
+      await Promise.all([copyFiles(), buildJSON(), buildScripts()]);
+      if (watch && browser === "chrome") {
+        await fs.outputFile(path.join(destDir, ".watch"), "");
+      }
+      process.stdout.write(chalk.green("✔\n"));
+    } catch (error) {
+      process.stdout.write(chalk.red("✘\n"));
+      if (error instanceof Error) {
+        process.stderr.write(`${error.toString()}\n`);
+      }
+      if (!watch) {
+        process.exitCode = 1;
+      }
     }
-    if (!context.watch) {
-      process.exitCode = 1;
-    }
-  }
-}
-
-function defineProcessEnv(
-  vars: Record<string, string>,
-): Record<string, string> {
-  return Object.fromEntries(
-    Object.entries(vars).map(([key, value]) => [
-      `process.env.${key}`,
-      JSON.stringify(value),
-    ]),
-  );
+  };
 }
 
 async function main() {
@@ -168,6 +241,14 @@ async function main() {
     })
     .parse(values);
 
+  const tempDir = await fs.mkdtemp(path.join(os.tmpdir(), "ublacklist-"));
+  process.on("exit", () => {
+    fs.removeSync(tempDir);
+  });
+  process.on("SIGINT", () => {
+    process.exit();
+  });
+
   const context = {
     browser,
     version,
@@ -175,20 +256,10 @@ async function main() {
     watch,
     srcDir: "src",
     destDir: `dist/${browser}${debug ? "-debug" : ""}`,
-    define: defineProcessEnv({
-      NODE_ENV: debug ? "development" : "production",
-      BROWSER: browser,
-      VERSION: version,
-      DEBUG: debug ? "true" : "false",
-      WATCH: watch ? "true" : "false",
-      DROPBOX_API_KEY: process.env.DROPBOX_API_KEY,
-      DROPBOX_API_SECRET: process.env.DROPBOX_API_SECRET,
-      GOOGLE_DRIVE_API_KEY: process.env.GOOGLE_DRIVE_API_KEY,
-      GOOGLE_DRIVE_API_SECRET: process.env.GOOGLE_DRIVE_API_SECRET,
-    }),
+    tempDir,
   };
-
-  await build(context);
+  const build = await createBuild(context);
+  await build();
   if (watch) {
     await new Promise((_resolve, reject) => {
       const limit = pLimit(1);
@@ -196,7 +267,7 @@ async function main() {
         .watch(context.srcDir, { ignoreInitial: true })
         .on("all", () => {
           if (limit.pendingCount === 0) {
-            limit(() => build(context));
+            limit(build);
           }
         })
         .on("error", reject);
