cb0172fe155860b2174b4eabb8c1863cd3ba5927
charmedsquints <144560989+charmedsquints@users.noreply.github.com>
2024-08-20 00:13:24 +0000

    Fix class name for results containing site links

--- src/scripts/search-engines/google-mobile.ts
+++ src/scripts/search-engines/google-mobile.ts
@@ -145,7 +145,7 @@ const mobileSerpHandlers: Record<string, SerpHandler> = {
             return null;
           }
           const webResultWithSiteLinks =
-            target.parentElement?.closest<HTMLElement>(".Ww4FFb.g, .mnr-c.g");
+            target.parentElement?.closest<HTMLElement>(".Ww4FFbs.vt6azsd");
           if (webResultWithSiteLinks) {
             return webResultWithSiteLinks;
           }
