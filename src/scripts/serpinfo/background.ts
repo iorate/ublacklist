066bded95f3ebf76b53929ec90fecb67936f5857
iorate <682043+iorate@users.noreply.github.com>
2025-05-18 18:00:55 +0900

    feat: Manifest V3 is now used on Firefox and Safari
    BREAKING CHANGE: Manifest V2 support has been dropped

--- src/scripts/serpinfo/background.ts
+++ src/scripts/serpinfo/background.ts
@@ -112,11 +112,7 @@ export function onStartup() {
 export function main() {
   addMessageFromTabListeners({
     "notify-blocked-result-count"(tabId: number, count: number) {
-      const action =
-        process.env.BROWSER === "chrome"
-          ? browser.action
-          : browser.browserAction;
-      action.setBadgeText({
+      browser.action.setBadgeText({
         tabId,
         text: count ? String(count) : "",
       });
