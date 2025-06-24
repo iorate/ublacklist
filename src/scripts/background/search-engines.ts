f3b9af8b1c1775f76178afaae26022e459c9d8b3
iorate <682043+iorate@users.noreply.github.com>
2025-06-24 23:21:33 +0900

    fix(safari): adjust content script registration

--- src/scripts/background/search-engines.ts
+++ src/scripts/background/search-engines.ts
@@ -2,18 +2,16 @@ import { GOOGLE_MATCHES } from "../../common/google-matches.ts";
 import { browser } from "../browser.ts";
 
 export async function registerContentScripts(): Promise<void> {
-  await browser.scripting.unregisterContentScripts();
-  return browser.scripting.registerContentScripts([
-    {
-      id: "serpinfo",
-      matches: ["*://*/*"],
-      excludeMatches: GOOGLE_MATCHES,
-      js: [
-        process.env.BROWSER === "safari"
-          ? "scripts/import-content-script.js"
-          : "scripts/serpinfo/content-script.js",
-      ],
-      runAt: "document_start",
-    },
-  ]);
+  if (process.env.BROWSER !== "safari") {
+    await browser.scripting.unregisterContentScripts();
+    return browser.scripting.registerContentScripts([
+      {
+        id: "serpinfo",
+        matches: ["*://*/*"],
+        excludeMatches: GOOGLE_MATCHES,
+        js: ["scripts/serpinfo/content-script.js"],
+        runAt: "document_start",
+      },
+    ]);
+  }
 }
