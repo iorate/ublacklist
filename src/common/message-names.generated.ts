62128a98842f6e81b2d13592d53df4dca4f12418
iorate <682043+iorate@users.noreply.github.com>
2025-12-20 18:46:35 +0900

    fix: relax size limit of browser sync (#708)
    * fix: relax size limit of browser sync
    
    * throw on hash collision
    
    * update message name for exceeded quota
    
    * remove mutex
    
    * update MAX_CHUNK_COUNT (effectively unreachable)

--- src/common/message-names.generated.ts
+++ src/common/message-names.generated.ts
@@ -181,6 +181,7 @@ export type MessageName0 =
   | "clouds_browserSync"
   | "clouds_browserSyncDescription"
   | "clouds_browserSyncTurnedOn"
+  | "clouds_browserSyncQuotaExceeded"
 
 export type MessageName1 =
   | "error"
